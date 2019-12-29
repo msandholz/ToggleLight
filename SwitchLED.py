6b59980a1ed701831e165956814548d0a57c21b1
msandholz <markus.sandholz@t-online.de>
2019-12-29 09:53:53 +0100

    Add files via upload

--- SwitchLED.py
+++ SwitchLED.py
@@ -1,16 +1,47 @@
-# coding=utf-8
-from __future__ import absolute_import
+#!/usr/bin/env python
+#coding: utf8
 
-import octoprint.plugin
 import os
+import RPi.GPIO as GPIO
+from time import sleep
 
-class SwitchLEDPlugin(octoprint.plugin.StartupPlugin):
-    def on_after_startup(self):
-        self._logger.info("SwitchLED on_after_start")
-		os.system('gpio export 7 out')
-		os.system('gpio -g write 7 1')
-    
-__plugin_name__ = "SwitchLED"
-__plugin_version__ = "1.0.0"
-__plugin_description__ = "SwitchLED Plugin"
-__plugin_implementation__ = SwitchLEDPlugin()
+# 1-wire
+#os.system('modprobe w1-gpio')
+#os.system('modprobe wi-therm')
+
+#base_dir = '\sys\bus\w1\devices'
+#device_folder = glob.glob(base_dir + '28*')[0]
+#device_file = device_folder + 'w1_slave'
+
+
+
+
+# GPIO settings
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+# Button
+btn = 18
+GPIO.setup(btn, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+# Check Light status
+status = 24
+GPIO.setup(status, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+# Light on/off
+light = 25
+GPIO.setup(light, GPIO.OUT)
+GPIO.output(light, False)
+
+print("SwitchLED service started!")
+
+while True:
+	led = GPIO.input(status)
+	
+	GPIO.wait_for_edge(btn, GPIO.RISING)
+	sleep(0.05)
+
+	GPIO.output(light, not led)
+	
+	GPIO.wait_for_edge(btn, GPIO.FALLING)
+	sleep(0.05)
